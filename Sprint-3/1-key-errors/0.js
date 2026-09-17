31df7faab2479373477ada64cf18ec7ac185fb1f
Bartosz Kawiak <bartoszkawiak.pt@gmail.com>
2026-09-17 18:06:45 +0100

    key error 0.js completed

--- Sprint-3/1-key-errors/0.js
+++ Sprint-3/1-key-errors/0.js
@@ -1,13 +1,24 @@
 // Predict and explain first...
-//  =============> write your prediction here
+/*  =============> I predict that the function would make 1st character uppercase and 
+then adds the rest of the string using slice. */
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
+
+//We cannot declare str again because it has already been declared as a parameter of the function.
+
+/* OLD CODE
 
 function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+  */
 
 // =============> write your explanation here
 // =============> write your new code here
+
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
