fa0eee29cead6f8e183f3f32b1e2b8216aff702a
Bartosz Kawiak <bartoszkawiak.pt@gmail.com>
2026-09-18 12:46:12 +0100

    3-mandatory-implement sprint3 completed

--- Sprint-3/3-mandatory-implement/2-cases.js
+++ Sprint-3/3-mandatory-implement/2-cases.js
@@ -14,3 +14,10 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+const str= "hello there"
+
+function strToUpperCase(str) {
+    return str.toUpperCase().replaceAll(" ","_")
+}
+console.log(strToUpperCase("lord of the rings"));
--- Sprint-3/3-mandatory-implement/3-to-pounds.js
+++ Sprint-3/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,46 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+// In Sprint-1, there is a program written in 3-mandatory-interpret/3-to-pounds.js
+
+// You will need to take this code and turn it into a reusable block of code.
+// You will need to declare a function called toPounds with an appropriately named parameter.
+
+// You should call this function a number of times to check it works for different inputs
+
+
+/*
+const penceString = "399p";
+
+const penceStringWithoutTrailingP = penceString.substring(
+  0,
+  penceString.length - 1
+);
+
+const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+const pounds = paddedPenceNumberString.substring(
+  0,
+  paddedPenceNumberString.length - 2
+);
+
+const pence = paddedPenceNumberString
+  .substring(paddedPenceNumberString.length - 2)
+  .padEnd(2, "0");
+
+console.log(`£${pounds}.${pence}`);
+*/
+
+function toPounds(penceString) {
+    let noP = penceString.substring(0,penceString.length -1)
+    let paddedP = noP.padStart(3,"0")
+    let pounds= paddedP.substring(0, paddedP.length -2)
+    
+    let pence= paddedP.substring(paddedP.length -2).padEnd(2,"0")
+
+    return `£${pounds}.${pence}`
+}
+console.log(toPounds("399p"))
+console.log(toPounds("5p"))
+console.log(toPounds("52p"))
+console.log(toPounds("1244p"))
