bb31521678854b6a2798e5f515f727481920e590
Bartosz Kawiak <bartoszkawiak.pt@gmail.com>
2026-09-18 11:29:17 +0100

    1-key-errors sprint3 completed

--- Sprint-3/1-key-errors/1.js
+++ Sprint-3/1-key-errors/1.js
@@ -1,10 +1,11 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> We will get a SyntaxError as decimalNumber has already been declared as a parameter of the function.
 
 // Try playing computer with the example to work out what is going on
 
+/*
 function convertToPercentage(decimalNumber) {
   const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
@@ -12,9 +13,17 @@ function convertToPercentage(decimalNumber) {
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(decimalNumber); */
 
 // =============> write your explanation here
+// We don't need to redeclare decimalNumber as it's value comes from function parameter, 
+// also console.log wont work as decimalNumber is only created inside the function. 
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+console.log(convertToPercentage(0.5));
--- Sprint-3/1-key-errors/2.js
+++ Sprint-3/1-key-errors/2.js
@@ -3,18 +3,22 @@
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> SyntaxError because function parameter must be a name not a number or value. The error occurs because 3 is a number.
 
+/*
 function square(3) {
     return num * num;
-}
+} */
 
-// =============> write the error message here
+// =============> SyntaxError: Unexpected number
 
-// =============> explain this error message here
+// =============> This error message is cause by trying to assign a number as a parameter, a parameter needs to be identifier such as num.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
-
+function square(num) {
+    return num * num;
+}
+console.log(square(5))
 
